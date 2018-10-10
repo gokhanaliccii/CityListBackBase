69257da9f038ba92ba0c47734a7b2753db788938
alici,gokhan <gokhanaliccii@gmail.com>
2018-10-10 20:49:44 +0300

    City and coordinate data model classes created

--- app/src/main/java/com/gokhanaliccii/citylist/data/model/City.java
+++ app/src/main/java/com/gokhanaliccii/citylist/data/model/City.java
@@ -1,4 +1,17 @@
 package com.gokhanaliccii.citylist.data.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class City {
+
+    @JsonProperty("_id")
+    private String id;
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("country")
+    private String country;
+    @JsonProperty("coord")
+    private Coordinate coord;
+
+
 }
