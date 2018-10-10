5b0f3fe69cb6dd1bc11703caa53672cae7e7ec7f
alici,gokhan <gokhanaliccii@gmail.com>
2018-10-10 22:13:34 +0300

    CityList recycler view adapter created

--- app/src/main/java/com/gokhanaliccii/citylist/ui/citylist/adapter/CityListAdapter.java
+++ app/src/main/java/com/gokhanaliccii/citylist/ui/citylist/adapter/CityListAdapter.java
@@ -1,9 +1,61 @@
 package com.gokhanaliccii.citylist.ui.citylist.adapter;
 
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.fasterxml.jackson.core.util.BufferRecycler;
+import com.gokhanaliccii.citylist.data.model.City;
+import com.gokhanaliccii.citylist.databinding.CardCityBinding;
 
-public class CityListAdapter {
+import java.util.List;
 
+public class CityListAdapter extends RecyclerView.Adapter<CityListAdapter.ViewHolder> {
 
+    private List<City> mCityList;
+
+    public CityListAdapter(List<City> mCityList) {
+        this.mCityList = mCityList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        LayoutInflater inflater = LayoutInflater.from(viewGroup.getContext());
+        CardCityBinding cityCardBinding = CardCityBinding.inflate(inflater, viewGroup, false);
+
+        return new ViewHolder(cityCardBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        City city = mCityList.get(position);
+        viewHolder.bind(city);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCityList == null
+                ? 0
+                : mCityList.size();
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        private CardCityBinding mCardBinding;
+
+        public ViewHolder(@NonNull CardCityBinding cardBinding) {
+            super(cardBinding.getRoot());
+
+            this.mCardBinding = cardBinding;
+        }
+
+        public void bind(City city) {
+            mCardBinding.setCity(city);
+        }
+
+    }
 
 }
